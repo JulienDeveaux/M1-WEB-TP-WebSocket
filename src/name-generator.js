2cb8c41caeef8dc482afc34387af326ee54e180e
Julien Deveaux <julien.deveaux@hotmail.com>
2022-02-22 16:42:07 +0100

    meilleurs couleurs

--- src/name-generator.js
+++ src/name-generator.js
@@ -9,7 +9,8 @@ import isDef from './is-def'
 
 var nouns = ['Circle', 'Cone', 'Cylinder', 'Ellipse', 'Hexagon', 'Irregular Shape', 'Octagon', 'Oval', 'Parallelogram', 'Pentagon', 'Pyramid', 'Rectangle', 'Semicircle', 'Sphere', 'Square', 'Star', 'Trapezoid', 'Triangle', 'Wedge', 'Whorl'];
 var adjectives = ['Amusing', 'Athletic', 'Beautiful', 'Brave', 'Careless', 'Clever', 'Crafty', 'Creative', 'Cute', 'Dependable', 'Energetic', 'Famous', 'Friendly', 'Graceful', 'Helpful', 'Humble', 'Inconsiderate', 'Likable', 'Middle Class', 'Outgoing', 'Poor', 'Practical', 'Rich', 'Sad', 'Skinny', 'Successful', 'Thin', 'Ugly', 'Wealth'];
-let colors = ['#2596be', '#eeeee4', '#e28743', '#eab676', '#76b5c5', '#21130d', '#873e23', '#abdbe3', '#063970', '#154c79', '#44bcd8', '#cce7e8', '#80391e', '#69bdd2', '#e07b39', '#ebb678', '#1874a5', '#cce7e8', '#195e83', '#44bcd8'];
+let colors = ["#28FF00", "#FCFF00", "#FF0000",
+    "#00FFF2", "#0050FF", "#E500FF"]
 
 export default (random) => {
     random = isDef(random) ? random : Math.random;
